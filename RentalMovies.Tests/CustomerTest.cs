756eeb95de190f195eb4008edb2645441c858195
Aleksey Kurbala <aleksej@kurbala.ru>
2011-10-01 23:28:02 +0400

    Added unit tests for htmlStatement() method

--- RentalMovies.Tests/CustomerTest.cs
+++ RentalMovies.Tests/CustomerTest.cs
@@ -13,6 +13,42 @@ namespace RentalMovies.Tests
                                       "Сумма задолженности составляет {3}\n" +
                                       "Вы заработали {4} очков за активность";
 
+        private string resultHtmlString = "<H1>Операции аренды для <EM>{0}" +
+                                          "</EM></H1><P>\n" +
+                                          "{1}:{2}<BR>\n" +
+                                          "{3}:{4}<BR>\n" +
+                                          "<P>Ваша задолженность составляет <EM>{5}</EM><P>\n" +
+                                          "На этой аренде вы заработали <EM>{6}" +
+                                          "</EM> очков за активность<P>";
+
+
+        [Test]
+        public void ReturnHtmlResult()
+        {
+            var CustomerName = "Customer 1";
+            var movieName = "Movie";
+            var customer = new Customer(CustomerName);
+            customer.AddRental(new Rental(
+                new Movie(movieName, 1), 10));
+
+            var movieName2 = "Movie2";
+            customer.AddRental(new Rental(
+                new Movie(movieName2, 1), 1));
+
+            var amount = 30;
+            var amount2 = 3;
+            var total = amount + amount2;
+
+            var renterPoints = 3;
+
+            var result = customer.htmlStatement();
+
+            Assert.AreEqual(string.Format(resultHtmlString,
+                CustomerName, movieName, amount, movieName2,
+                amount2, total, renterPoints), result);
+        }
+
+        
         [Test]
         public void ReturnResultIfMovieIsNewReleaseAndDaysRentedMoreThanOne()
         {
