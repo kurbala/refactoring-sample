c07b5fd299b95186a25fe133701ae2be719b7c9d
Aleksey Kurbala <aleksej@kurbala.ru>
2011-10-02 12:00:38 +0400

    Move GetCharge() method from Movie class to Price

--- RentalMovies/Movie.cs
+++ RentalMovies/Movie.cs
@@ -52,24 +52,7 @@ namespace RentalMovies
 
         public double GetCharge(int daysRented)
         {
-            double result = 0;
-            switch (PriceCode)
-            {
-                case Movie.REGULAR:
-                    result += 2;
-                    if (daysRented > 2)
-                        result += (daysRented - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    result += daysRented * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    result += 1.5;
-                    if (daysRented > 3)
-                        result += (daysRented - 3) * 1.5;
-                    break;
-            }
-            return result;
+            return _price.GetCharge(daysRented);
         }
 
         public int GetFrequentRenterPoints(int daysRented)
